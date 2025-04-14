1cc8694afc66e5e06badb2f7237ad8ade145c957
Camila-Ailen <camila.gomez@fceqyn.unam.edu.ar>
2025-04-14 19:06:05 -0300

    ejercicio 10 y 11

---
+++ Laboratorio1/10.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+// Variable global
+var x int
+
+// Mutex para proteger el acceso a x
+var mutex sync.Mutex
+
+// Función para incrementar x
+func incrementar() {
+	mutex.Lock()         // Bloquear el acceso a x
+	defer mutex.Unlock() // Liberar el bloqueo
+	x += 5               // Incrementar x
+}
+
+func diez() {
+	// Crear un WaitGroup para esperar a que todas las goroutines terminen
+	var wg sync.WaitGroup
+
+	// Lanzar 100 goroutines
+	for i := 0; i < 10; i++ {
+		wg.Add(1) // Incrementar el contador del WaitGroup
+		go func() {
+			defer wg.Done() // Decrementar el contador al finalizar
+			incrementar()
+		}()
+	}
+
+	// Esperar a que todas las goroutines terminen
+	wg.Wait()
+
+	// Imprimir el valor final de x
+	fmt.Printf("El valor final de x es: %d\n", x)
+}
---
+++ Laboratorio1/11.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+// Mutex global
+var mx sync.Mutex
+var mx2 sync.Mutex
+
+// Función b()
+func b() {
+	mx2.Lock()         // Bloquear el mutex
+	defer mx2.Unlock() // Desbloquear el mutex al finalizar
+	fmt.Println("Hola mundo")
+}
+
+// Función a()
+func a() {
+	mx.Lock()         // Bloquear el mutex
+	defer mx.Unlock() // Desbloquear el mutex al finalizar
+	b()               // Invocar a la función b()
+}
+
+func main() {
+	// Invocar a la función a() desde la gorutina principal
+	a()
+}
