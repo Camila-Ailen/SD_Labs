90fd79192412e469aad3c2e030afa5737db95c83
franco henker <francohenker@gmail.com>
2025-05-16 11:08:54 -0300

    readme de los integrantes del grupo agregado

--- grpc-pg-1/cliente/main.go
+++ grpc-pg-1/cliente/main.go
@@ -4,9 +4,9 @@ import (
 	"context"
 	"grpc-pg-1/proto"
 	"log"
-	"time"
 	"sync"
-	
+	"time"
+
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
@@ -16,21 +16,21 @@ var nombres = []string{"Juan", "Maria", "Pedro", "Ana", "Luis", "Claudio", "Sofi
 func main() {
 	var wg sync.WaitGroup
 	var mu sync.Mutex
-	
+
 	conn, err := grpc.NewClient("localhost:8000", grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		log.Fatalf("No se pudo conectar: %v", err)
 	}
 	defer conn.Close()
 	c := proto.NewServicioClient(conn)
-	
+
 	for i := 0; i < 1000; i++ {
 		wg.Add(1)
 		go func(nombre string) {
 			defer wg.Done()
 			mu.Lock()
 			defer mu.Unlock()
-			
+
 			ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 			defer cancel()
 			r, err := c.Hola(ctx, &proto.Requerimiento{Nombre: nombre})
